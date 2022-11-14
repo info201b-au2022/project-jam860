20ea8675069ac6f7f2e6829bff0e78d1c0262fdb
James <jam860@uw.edu>
2022-11-13 17:06:02 -0800

    update

--- source/chart 1.R
+++ source/chart 1.R
@@ -14,9 +14,16 @@ data <- data_all %>%
   group_by(self_care) %>%
   summarize(students = length(self_care)) %>%
   filter(students >= 50) %>%
-  arrange(desc(students))
+  arrange(desc(students)) 
 
-ggplot(data, aes(x = reorder(self_care, students), y = students)) +
+chart_1 <- ggplot(data, aes(x = reorder(self_care, students), y = students)) +
   labs(y = "Students", x = "Methods of Self Care") +
   geom_bar(fill= "pink", color = "pink", stat ="identity") +
   coord_flip()
+
+# I included this chart because I wanted to know the most popular ways in which
+# students tended to their mental health through self-care. We believe that 
+# there are many ways to take care of one's mental health that they can do either
+# at home or by themselves. This bar graph reveals the top 7 methods that people 
+# do to take care of their mental health. I also chose a horizontal bar graph to
+# make the groups more readable. In addition, 
