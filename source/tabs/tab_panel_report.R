6851800836edf77de7f7587f1ebc027e37a733bf
James <jam860@uw.edu>
2022-12-07 11:22:42 -0800

    changed a couple of things

--- source/tabs/tab_panel_report.R
+++ source/tabs/tab_panel_report.R
@@ -7,18 +7,8 @@ tab_panel_report <-tabPanel(
   "Report",
 
   fluidRow(
-    column(
       6,
       includeMarkdown("tabs/report.md")
-    ),
-    column(
-      3,
-      p("This is a paragraph.")
-    ),
-    column(
-      3,
-      p("Rightmost column.")
-    )
   )
 
 
